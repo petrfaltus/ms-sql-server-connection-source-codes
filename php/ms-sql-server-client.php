dcbc66ed799e6fd3c7da0188837297f6540f70a2
Petr Faltus <petrfaltus@petrfaltus.net>
2020-08-24 10:57:38 +0200

    PHP - the full SELECT statement

--- php/ms-sql-server-client.php
+++ php/ms-sql-server-client.php
@@ -22,6 +22,7 @@ $db_port = 1433;
 $db_name = "testdb";
 $db_username = "testuser";
 $db_password = "T3stUs3r!";
+$db_table = "animals";
 
 $availableDrivers = PDO::getAvailableDrivers();
 
@@ -48,6 +49,21 @@ try
    print_r($conn->getAttribute(PDO::ATTR_SERVER_INFO));
    echo "ATTR_SERVER_VERSION = ".$conn->getAttribute(PDO::ATTR_SERVER_VERSION).PHP_EOL;
    echo PHP_EOL;
+
+   // Full SELECT statement
+   $stm1 = $conn->prepare("select * from ".$db_table);
+   $stm1->execute();
+   echo "Total columns: ".$stm1->columnCount().PHP_EOL;
+
+   echo "Fetch all rows ";
+   $lines1 = $stm1->fetchAll(PDO::FETCH_ASSOC);
+   if ($lines1 == false)
+     print_r($stm1->errorInfo());
+   else
+     print_r($lines1);
+   echo PHP_EOL;
+
+   $stm1 = null;
 
    // Disconnect the database
    $conn = null;
