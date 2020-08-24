05cd48dce6dc913bac354b4f98def01a76351b8c
Petr Faltus <petrfaltus@petrfaltus.net>
2020-08-24 09:58:46 +0200

    Java/Maven - the full SELECT statement

--- java-maven/src/main/java/cz/petrfaltus/ms_sql_server/Program.java
+++ java-maven/src/main/java/cz/petrfaltus/ms_sql_server/Program.java
@@ -4,7 +4,10 @@ import static java.lang.System.out;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import java.util.Enumeration;
 import java.util.Properties;
@@ -18,6 +21,8 @@ public class Program {
 	private static final String DB_NAME = "testdb";
 	private static final String DB_USERNAME = "testuser";
 	private static final String DB_PASSWORD = "T3stUs3r!";
+
+	private static final String DB_TABLE = "animals";
 
 	public static void main(String[] args) {
 		try {
@@ -35,6 +40,31 @@ public class Program {
 				String value = connInfo.getProperty(key);
 				out.println(key + " : " + value);
 			}
+
+			// Full SELECT statement
+			Statement stm1 = conn.createStatement();
+			ResultSet rs1 = stm1.executeQuery("select * from " + DB_TABLE);
+			ResultSetMetaData rsmd1 = rs1.getMetaData();
+
+			int columns1 = rsmd1.getColumnCount();
+			out.println("Total columns: " + columns1);
+			for (int ii = 1; ii <= columns1; ii++) {
+				out.println(" - " + rsmd1.getColumnName(ii) + " " + rsmd1.getColumnTypeName(ii) + " (" + rsmd1.getPrecision(ii) + ")");
+			}
+
+			int rowNumber1 = 0;
+			while (rs1.next()) {
+				++rowNumber1;
+
+				out.print(rowNumber1 + ")");
+
+				for (int ii = 1; ii <= columns1; ii++) {
+					out.print(" '" + rs1.getObject(ii) + "'");
+				}
+
+				out.println();
+			}
+			out.println();
 
 			// Disconnect the database
 			conn.close();
