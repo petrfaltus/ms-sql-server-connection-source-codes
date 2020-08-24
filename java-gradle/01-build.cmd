e8b9c7da840f10f3a48279aefe26aeb08f8421bc
Petr Faltus <petrfaltus@petrfaltus.net>
2020-08-24 10:11:32 +0200

    Java/Gradle - create the empty console application

---
+++ java-gradle/01-build.cmd
@@ -0,0 +1,5 @@
+@echo off
+
+call 03-clean.cmd
+
+call gradle.bat assemble build
---
+++ java-gradle/src/main/java/cz/petrfaltus/ms_sql_server/Program.java
@@ -0,0 +1,11 @@
+package cz.petrfaltus.ms_sql_server;
+
+import static java.lang.System.out;
+
+public class Program {
+	public static void main(String[] args) {
+		out.println("OK");
+
+	}
+
+}
