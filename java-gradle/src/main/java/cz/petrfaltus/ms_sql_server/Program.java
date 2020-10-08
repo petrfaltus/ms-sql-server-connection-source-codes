8e74e1628b64693162dc5b10cdf7eb3c07b757e2
Petr Faltus <petrfaltus@petrfaltus.net>
2020-10-08 18:01:05 +0200

    Java/Gradle - SELECT function statement

--- java-gradle/src/main/java/cz/petrfaltus/ms_sql_server/Program.java
+++ java-gradle/src/main/java/cz/petrfaltus/ms_sql_server/Program.java
@@ -36,6 +36,10 @@ public class Program {
 
 	private static final String DB_TOTAL_NAME = "total";
 
+	private static final int DB_FACTORIAL_VALUE = 4;
+
+	private static final String DB_RESULT_NAME = "result";
+
 	private static String getNow()
 	{
 		Date date = new Date();
@@ -117,6 +121,32 @@ public class Program {
 
 				for (int ii = 1; ii <= columns2; ii++) {
 					out.print(" '" + rs2.getObject(ii) + "'");
+				}
+
+				out.println();
+			}
+			out.println();
+
+			// SELECT function statement
+			PreparedStatement stm3 = conn.prepareStatement("select dbo.factorial(?) as " + DB_RESULT_NAME);
+			stm3.setInt(1, DB_FACTORIAL_VALUE);
+			ResultSet rs3 = stm3.executeQuery();
+			ResultSetMetaData rsmd3 = rs3.getMetaData();
+
+			int columns3 = rsmd3.getColumnCount();
+			out.println("Total columns: " + columns3);
+			for (int ii = 1; ii <= columns3; ii++) {
+				out.println(" - " + rsmd3.getColumnName(ii) + " " + rsmd3.getColumnTypeName(ii) + " (" + rsmd3.getPrecision(ii) + ")");
+			}
+
+			int rowNumber3 = 0;
+			while (rs3.next()) {
+				++rowNumber3;
+
+				out.print(rowNumber3 + ")");
+
+				for (int ii = 1; ii <= columns3; ii++) {
+					out.print(" '" + rs3.getObject(ii) + "'");
 				}
 
 				out.println();
